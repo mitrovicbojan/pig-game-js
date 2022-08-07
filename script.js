089f50a5d596df214905db0f9bfc623331358454
unknown <mitrovicbojan87@gmail.com>
2022-08-07 18:43:54 +0300

    new game

--- script.js
+++ script.js
@@ -13,15 +13,25 @@ const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
 //starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0]; //storing total scores
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0]; //storing total scores
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
 
+  diceEl.classList.add('hidden');
+};
+init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -78,4 +88,7 @@ btnHold.addEventListener('click', function () {
   }
   //finish the game
   //switch to the next player
+});
+btnNew.addEventListener('click', function () {
+  init();
 });
